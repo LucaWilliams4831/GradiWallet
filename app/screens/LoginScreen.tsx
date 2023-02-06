e8c07824ebe2f73fdfbb2b928372f117e0845986
LucaWilliams4831 <lucaw4831@gmail.com>
2023-02-06 22:14:26 +0300

    keplr module add

--- app/screens/LoginScreen.tsx
+++ app/screens/LoginScreen.tsx
@@ -1,4 +1,3 @@
-import { observer } from "mobx-react-lite"
 import React, {useState, FC, useEffect } from "react"
 import { Dimensions, View, Image, ImageStyle,  TextStyle, ViewStyle } from "react-native"
 import { Button, Icon, Screen, Text} from "../components"
@@ -6,90 +5,203 @@ import { useStores } from "../models"
 import { AppStackScreenProps } from "../navigators"
 import { colors, spacing } from "../theme"
 import  "elliptic";
-
 import Web3Auth, { LOGIN_PROVIDER, OPENLOGIN_NETWORK, State } from "@web3auth/react-native-sdk";
 import { Buffer } from "buffer";
-import Constants, { AppOwnership } from "expo-constants";
-import * as Linking from "expo-linking";
-// import { StatusBar } from "expo-status-bar";
+import { observer } from "mobx-react-lite"
+
+
 import * as WebBrowser from "expo-web-browser";
+import { RouteProp, useNavigation, useRoute } from "@react-navigation/native";
+import { Controller, useForm } from "react-hook-form";
+import NodeDetailManager from "@toruslabs/fetch-node-details";
+import Torus from "@toruslabs/torus.js";
 
 
-// import * as WebBrowser from "expo-web-browser";
+
+interface FormData {
+  name: string;
+  password: string;
+  confirmPassword: string;
+}
+const useTorusGoogleSignIn = (): {
+  privateKey: Uint8Array | undefined;
+  email: string | undefined;
+} => {
+  const [privateKey, setPrivateKey] = useState<Uint8Array | undefined>();
+  const [email, setEmail] = useState<string | undefined>();
+
+
+  const naviagtion = useNavigation();
+
+  useEffect(() => {
+    
+
+    (async () => {
+      try {
+        console.log("luca log started")
+        const nonce: string = Math.floor(Math.random() * 10000).toString();
+        const state = encodeURIComponent(
+          Buffer.from(
+            JSON.stringify({
+              instanceId: nonce,
+              redirectToOpener: false,
+            })
+          ).toString("base64")
+        );
+
+        const finalUrl = new URL(
+          "https://accounts.google.com/o/oauth2/v2/auth"
+        );
+        finalUrl.searchParams.append("response_type", "token id_token");
+        finalUrl.searchParams.append(
+          "client_id",
+          "413984222848-8r7u4ip9i6htppalo6jopu5qbktto6mi.apps.googleusercontent.com"
+        );
+        finalUrl.searchParams.append("state", state);
+        finalUrl.searchParams.append("scope", "profile email openid");
+        finalUrl.searchParams.append("nonce", nonce);
+        finalUrl.searchParams.append("prompt", "consent select_account");
+        finalUrl.searchParams.append(
+          "redirect_uri",
+          "https://oauth.keplr.app/google.html"
+        );
+
+        const result = await WebBrowser.openAuthSessionAsync(
+          finalUrl.href,
+          "app.keplr.oauth://"
+        );
+        if (result.type !== "success") {
+          throw new Error("Failed to get the oauth");
+        }
+
+        if (!result.url.startsWith("app.keplr.oauth://google#")) {
+          throw new Error("Invalid redirection");
+        }
+
+        const redirectedUrl = new URL(result.url);
+        const paramsString = redirectedUrl.hash;
+        const searchParams = new URLSearchParams(
+          paramsString.startsWith("#") ? paramsString.slice(1) : paramsString
+        );
+        if (state !== searchParams.get("state")) {
+          throw new Error("State doesn't match");
+        }
+        const idToken = searchParams.get("id_token");
+        const accessToken = searchParams.get("access_token");
+
+        const userResponse = await fetch(
+          "https://www.googleapis.com/userinfo/v2/me",
+          {
+            method: "GET",
+            headers: {
+              Authorization: `Bearer ${accessToken || idToken}`,
+            },
+          }
+        );
+
+        if (userResponse.ok) {
+          const userInfo: {
+            picture: string;
+            email: string;
+            name: string;
+          } = await userResponse.json();
+
+          const { email } = userInfo;
+
+          const nodeDetailManager = new NodeDetailManager({
+            network: "mainnet",
+            proxyAddress: "0x638646503746d5456209e33a2ff5e3226d698bea",
+          });
+          const {
+            torusNodeEndpoints,
+            torusNodePub,
+            torusIndexes,
+          } = await nodeDetailManager.getNodeDetails({
+            verifier: "chainapsis-google",
+            verifierId: email.toLowerCase(),
+          });
+
+          const torus = new Torus();
+
+          const response = await torus.getPublicAddress(
+            torusNodeEndpoints,
+            torusNodePub,
+            {
+              verifier: "chainapsis-google",
+              verifierId: email.toLowerCase(),
+            },
+            true
+          );
+          const data = await torus.retrieveShares(
+            torusNodeEndpoints,
+            torusIndexes,
+            "chainapsis-google",
+            {
+              verifier_id: email.toLowerCase(),
+            },
+            (idToken || accessToken) as string
+          );
+          if (typeof response === "string")
+            throw new Error("must use extended pub key");
+          if (
+            data.ethAddress.toLowerCase() !== response.address.toLowerCase()
+          ) {
+            throw new Error("data ethAddress does not match response address");
+          }
+
+          setPrivateKey(Buffer.from(data.privKey.toString(), "hex"));
+          setEmail(email);
+        } else {
+          throw new Error("Failed to fetch user data");
+        }
+      } catch (e) {
+        console.log(e);
+        naviagtion.goBack();
+      } finally {
+     
+      }
+    })();
+
+    
+  }, []);
+
+  return {
+    privateKey,
+    email,
+  };
+};
+
 
 interface LoginScreenProps extends AppStackScreenProps<"Login"> {}
 
 export const LoginScreen: FC<LoginScreenProps> = observer(function LoginScreen(_props) {
   
-  global.Buffer = global.Buffer || Buffer;
-
-  
-
-
-    
-
   const $iconStyle: ImageStyle = { width: 30, height: 30 }
   
   const welcomeLogo = require("../../assets/images/logo.png")
   
-  const {
-    authenticationStore: {
-      setAuthEmail,
-      setAuthPassword,
-    },
-  } = useStores()
+  const { privateKey, email } =  useTorusGoogleSignIn();
 
   useEffect(() => {
     // Here is where you could fetch credentials from keychain or storage
     // and pre-fill the form fields.
-    setAuthEmail("ignite@infinite.red")
-    setAuthPassword("ign1teIsAwes0m3")
+
 
   }, [])
 
   const clientId ="BJ3krf9XzUTs5YTOKjI8QO0T36e43xDO6FNgssCFtVQMkgjhvPSZHmlY2q_SLWjZ_LS4JgVxGtw5dI-xZvyP6nc";
-  const [key, setKey] = useState('');
-  const [userInfo, setUserInfo] = useState('');
-  
   const scheme = "GradiWallet";
-  const resolvedRedirectUrl = `${scheme}://openlogin`;
-  const uiConsole = (...args) => {
-   // setConsole(JSON.stringify(args || {}, null, 2) + '\n\n\n\n' + console);
-  };
-
+  
   const login = async () => {
     console.log('Logged In Luca start');
-    try {
-      const web3auth = new Web3Auth(WebBrowser, {
-        clientId,
-        network: OPENLOGIN_NETWORK.CYAN, // or other networks
-      });
-      const info = await web3auth.login({
-        loginProvider: LOGIN_PROVIDER.GOOGLE,
-        redirectUrl: resolvedRedirectUrl,
-        mfaLevel: 'default',
-        curve: 'secp256k1',
-      });
-
-      
-      // console.log(info.privKey)
-      
-      // setKey(info.privKey);
-      console.log('Logged In Luca', info.privKey);
-    } catch (e) {
-      // console.log('Logged errors herere', e);
-      console.log("luca exception logs")
+    if (!privateKey || !email) {
+      return;
     }
-  }
+    console.log('Logged In Luca start',privateKey, email );
+
+ }
 
  
-  useEffect(() => {
-    return () => {
-      setAuthPassword("")
-      setAuthEmail("")
-    }
-  }, [])
-
   return (
     <Screen
       preset="auto"
